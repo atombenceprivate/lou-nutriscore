86e3ea6d4c450925aa95aea79843082eb7f24e15
Barna Bence <149047144+atombenceprivate@users.noreply.github.com>
2026-09-09 16:34:04 +0200

    feat: complete roadmap phase

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { ScoreConfigError, ScoreInputError } from "./errors.js";
 export { createCalculator, createCompositeCalculator, validateConfig } from "./engine.js";
 export { balancedPreset, exportPreset, importPreset, migratePreset } from "./presets.js";
+export { confidence, createAuditTrail, createCalculatorFromPreset, evaluateConstraints, generateConfigSchema, generateInputSchema, listPresets, normalizeDataset, rank, registerPreset, scoreStream, simulateWeights, statistics, top } from "./insights.js";
 export type * from "./types.js";
