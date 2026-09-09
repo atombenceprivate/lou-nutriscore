3643d3696de16bc27f663e2bb0c75632913bb48d
Barna Bence <149047144+atombenceprivate@users.noreply.github.com>
2026-09-09 15:41:50 +0200

    chore: add src/index.ts

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+export { ScoreConfigError, ScoreInputError } from "./errors.js";
+export { createCalculator, validateConfig } from "./engine.js";
+export { balancedPreset, exportPreset, importPreset } from "./presets.js";
+export type * from "./types.js";
