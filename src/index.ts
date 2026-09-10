27fd86cccc52c8596d950e37500b853a253d8d5b
Barna Bence <149047144+atombenceprivate@users.noreply.github.com>
2026-09-10 12:18:42 +0200

    feat: add v0.5 developer experience and Lou Universe

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ export { ScoreConfigError, ScoreInputError } from "./errors.js";
 export { createCalculator, createCompositeCalculator, validateConfig } from "./engine.js";
 export { balancedPreset, exportPreset, importPreset, migratePreset } from "./presets.js";
 export { confidence, createAuditTrail, createCalculatorFromPreset, evaluateConstraints, generateConfigSchema, generateInputSchema, listPresets, normalizeDataset, rank, registerPreset, scoreStream, simulateWeights, statistics, top } from "./insights.js";
+export { defineExtension, installExtension } from "./sdk.js";
+export { createScoreViewModel } from "./ui.js";
 export type * from "./types.js";
